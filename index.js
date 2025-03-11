7620ef63a147b0ea400fcc4fbbe05192f11e89b6
paul-laird <35932444+paul-laird@users.noreply.github.com>
2025-03-11 12:26:27 +0000

    added user routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import userRoutes from './routes/users.js'
 
 const app = express();
 const PORT = 8080
 
 app.use(bodyParser.json());
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {
     console.log('[GET ROUTE]');
